d7413a1991a77cb189ad4618e095712b12bce733
lee <ce.li@thoughtworks.com>
2020-06-17 23:09:51 +0800

    13

--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -4,8 +4,6 @@ jest.mock("axios");
 
 describe("users", () => {
   test("should get users data with mock axios get", async () => {
-    // TODO 13: add async test with manual mock
-
     await expect(getUsers()).resolves.toEqual({});
   });
 });
