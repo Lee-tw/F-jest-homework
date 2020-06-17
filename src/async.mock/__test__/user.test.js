90dca1ee4aa04f3847fc043a246466dcf4732f66
lee <ce.li@thoughtworks.com>
2020-06-17 23:27:12 +0800

    19 - 20

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,14 +1,27 @@
 import { register } from "../user";
+import { verifyPassword, verifyUsername } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
-    // TODO 19: add test here
+  test("should post user when validated", async () => {
+    const username = "mock username";
+    const password = "mock password";
+
+    const result = register(username, password);
+    await expect(result).resolves.toEqual({});
   });
 
-  test("should reject with Error when username is invalid", () => {
-    // TODO 20: add test here
+  test("should reject with Error when username is invalid", async () => {
+    const username = "mock username";
+    const password = "mock password";
+
+    verifyPassword.mockImplementation(() => false);
+
+    const result = register(username, password);
+    await expect(result).rejects.toEqual(
+      new Error("wrong username or password")
+    );
   });
 });
