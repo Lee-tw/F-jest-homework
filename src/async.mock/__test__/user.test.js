327fd5f05bccec0cb7c85feb3c15ac7ae4a399d4
lee <ce.li@thoughtworks.com>
2020-06-17 23:35:46 +0800

    14

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,5 +1,5 @@
 import { register } from "../user";
-import { verifyPassword, verifyUsername } from "../verify";
+import { verifyPassword } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
